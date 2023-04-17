b9fc3fc83ae045267f38b32c57417254b2a58cad
AnyStepanyan <a.stepaniann@gmail.com>
2023-04-17 22:03:51 +0400

    feat: add

--- src/file-names.js
+++ src/file-names.js
@@ -15,7 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
+function renameFiles( names ) {
   const repeated = {};
   return names.map(name => {
       let newName = name;
